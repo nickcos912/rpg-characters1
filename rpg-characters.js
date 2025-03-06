0e9dec3a6f0975db25d39e846af67f8b7895ab61
nickcos912 <npc5452@psu.edu>
2025-03-06 13:07:24 -0500

    x

--- rpg-characters.js
+++ rpg-characters.js
@@ -80,5 +80,52 @@ export class RpgCharacters extends DDDSuper(I18NMixin(LitElement)) {
       .href;
   }
 }
+render() {
+  return html`
+  <h2>${this.title}</h2>
+  <details open>
+    <summary>Search inputs</summary>
+    <div>
+      <input id="input" placeholder="Search NASA images" @input="${this.inputChanged}" />
+    </div>
+  </details>
+  <div class="results">
+    ${this.items.map((item, index) => html`
+    <nasa-image
+      source="${item.links[0].href}"
+      title="${item.data[0].title}"
+    ></nasa-image>
+    `)}
+  </div>
+  `;
+}
 
+inputChanged(e) {
+  this.value = this.shadowRoot.querySelector('#input').value;
+}
+// life cycle will run when anything defined in `properties` is modified
+updated(changedProperties) {
+  // see if value changes from user input and is not empty
+  if (changedProperties.has('value') && this.value) {
+    this.updateResults(this.value);
+  }
+  else if (changedProperties.has('value') && !this.value) {
+    this.items = [];
+  }
+  // @debugging purposes only
+  if (changedProperties.has('items') && this.items.length > 0) {
+    console.log(this.items);
+  }
+}
+
+updateResults(value) {
+  this.loading = true;
+  fetch(`https://images-api.nasa.gov/search?media_type=image&q=${value}`).then(d => d.ok ? d.json(): {}).then(data => {
+    if (data.collection) {
+      this.items = [];
+      this.items = data.collection.items;
+      this.loading = false;
+    }  
+  });
+}
 globalThis.customElements.define(RpgCharacters.tag, RpgCharacters);
